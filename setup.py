3ea9316dc607af55a3e2d9d86ac0214702038581
Michel Albert <michel@albert.lu>
2011-12-11 14:16:55 +0100

    Modified the setup script to autom. install the configs

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from os.path import join, exists
 
 from setuptools import setup, find_packages
 
@@ -8,17 +10,24 @@ AUTHOR = "Michel Albert"
 AUTHOR_EMAIL = "michel@albert.lu"
 VERSION = __import__(PACKAGE).__version__
 
+if not exists('/usr/share/grc/conf.d'):
+    os.makedirs('/usr/share/grc/conf.d')
+
 setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=open("README.rst").read(),
+    long_description=open("docs/README.rst").read(),
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license="GPL",
     install_requires = [
         'pyyaml'
       ],
+    data_files=[
+        ('/usr/share/grc/conf.d', [join('examples', _) for _ in
+            os.listdir('examples') if _[-1] != '~'])
+        ],
     scripts = ['grc/scripts/grc'],
     packages=find_packages(exclude=["tests.*", "tests"]),
     zip_safe=False,
