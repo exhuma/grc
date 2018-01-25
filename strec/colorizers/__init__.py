f8d7177b5b42027f12e0f39e2a1c568a420c7ac3
Michel Albert <michel@albert.lu>
2018-01-25 18:00:47 +0100

    Add find_conf to Colorizer class

--- strec/colorizers/__init__.py
+++ strec/colorizers/__init__.py
@@ -5,45 +5,45 @@ from os.path import exists, join
 from grc import CONF_LOCATIONS
 
 
-def find_conf(appname):
-    """
-    Searches for a config file name.
-
-    Search order:
-        ~/.grc/conf.d/<appname>.yml
-        /etc/grc/conf.d/<appname>.yml
-        /usr/share/grc/conf.d/<appname>.yml
-
-    TIP:
-        If you have one config file that could be used for multiple
-        applications: symlink it!
-    """
-    for folder in CONF_LOCATIONS:
-        confname = join(folder, "%s.yml" % appname)
-        if exists(confname):
-            return confname
-
-    raise FileNotFoundError("No config found named '%s.yml'\n"
-                            'Resolution order:\n   %s\n' % (
-                                appname,
-                                ',\n   '.join(CONF_LOCATIONS)))
-
-
 class Colorizer:
 
     @staticmethod
     def from_config(config_name):
-        filename = find_conf(config_name)
+        filename = Colorizer.find_conf(config_name)
         with open(filename) as fptr:
             conf = load(fptr, Loader=SafeLoader)
         return Colorizer(conf)
 
     @staticmethod
     def from_basename(basename):
-        filename = find_conf(basename)
+        filename = Colorizer.find_conf(basename)
         with open(filename) as fptr:
             conf = load(fptr, Loader=SafeLoader)
         return Colorizer(conf)
+
+    @staticmethod
+    def find_conf(appname):
+        """
+        Searches for a config file name.
+
+        Search order:
+            ~/.grc/conf.d/<appname>.yml
+            /etc/grc/conf.d/<appname>.yml
+            /usr/share/grc/conf.d/<appname>.yml
+
+        TIP:
+            If you have one config file that could be used for multiple
+            applications: symlink it!
+        """
+        for folder in CONF_LOCATIONS:
+            confname = join(folder, "%s.yml" % appname)
+            if exists(confname):
+                return confname
+
+        raise FileNotFoundError("No config found named '%s.yml'\n"
+                                'Resolution order:\n   %s\n' % (
+                                    appname,
+                                    ',\n   '.join(CONF_LOCATIONS)))
 
     def __init__(self, conf):
         self.state = ['root']
