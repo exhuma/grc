6f8275c50417ce869763a228b040559db9f4b4f9
Michel Albert <michel@albert.lu>
2021-10-14 20:31:49 +0200

    Apply black

--- strec/__init__.py
+++ strec/__init__.py
@@ -5,7 +5,7 @@ from .version import VERSION
 __version__ = VERSION
 
 CONF_LOCATIONS = [
-    join(expanduser('~'), '.strec', 'conf.d'),
-    join('/etc', 'strec', 'conf.d'),
-    join('/usr', 'share', 'strec', 'conf.d')
+    join(expanduser("~"), ".strec", "conf.d"),
+    join("/etc", "strec", "conf.d"),
+    join("/usr", "share", "strec", "conf.d"),
 ]
