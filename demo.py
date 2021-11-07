3a2ef5ada10aa8d538462d706687d14b8fd2dcc4
Michel Albert <michel@albert.lu>
2021-11-07 17:57:57 +0100

    Implement replace=<string>

--- demo.py
+++ demo.py
@@ -24,11 +24,22 @@ parser = g.Parser(
             ["default"],
             g.Count.UNBLOCK,
         ),
+        g.Rule(
+            r"\b(\w+) line\b",
+            ["blue"],
+            count=g.Count.MORE,
+            replace=r"hello \1 world line",
+        ),
+        g.Rule(
+            r"(line)",
+            ["yellow"],
+            count=g.Count.MORE,
+        ),
     ],
     sys.stdout,
     g.ANSI,
 )
 
-for n in range(10):
+for n in range(30):
     line = sys.stdin.readline()
     parser.feed(line)
