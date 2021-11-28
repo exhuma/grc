22b642d07a8a03a25e17fe3b18f9ccb3d0d637ea
Michel Albert <michel@albert.lu>
2021-11-28 14:57:49 +0100

    Integrate garabik-style parser into main code

--- strec/core.py
+++ strec/core.py
@@ -15,10 +15,10 @@ from os.path import basename, exists, join
 
 import pexpect
 import pkg_resources
-from blessings import Terminal
 
 from strec import CONF_LOCATIONS
 from strec.colorizers import Colorizer
+from strec.garabik import ANSI, Parser, parse_config
 
 # Add the installation folder to the config search path
 CONF_LOCATIONS.append(pkg_resources.resource_filename("strec", "../configs"))
@@ -46,7 +46,7 @@ def parse_args(args):
     return parser.parse_args(args)
 
 
-def find_conf(appname):
+def find_conf(file_or_app_name):
     """
     Searches for a config file name.
 
@@ -59,25 +59,33 @@ def find_conf(appname):
         If you have one config file that could be used for multiple
         applications: symlink it!
     """
+    if exists(file_or_app_name):
+        return file_or_app_name
+
     for folder in CONF_LOCATIONS:
-        confname = join(folder, "%s.yml" % appname)
+        confname = join(folder, "%s.yml" % file_or_app_name)
         if exists(confname):
+            if confname.endswith(".yml") or confname.endswith(".yaml"):
+                raise NotImplementedError(
+                    "YAML config files are not yet supported"
+                )
             return confname
 
     sys.stderr.write(
         "No config found named '%s.yml'\n"
-        "Resolution order:\n   %s\n" % (appname, ",\n   ".join(CONF_LOCATIONS))
+        "Resolution order:\n   %s\n"
+        % (file_or_app_name, ",\n   ".join(CONF_LOCATIONS))
     )
     sys.exit(9)
 
 
 def load_config(config_name):
     with open(find_conf(config_name)) as fptr:
-        conf = load(fptr, Loader=SafeLoader)
-    return conf
+        rules = parse_config(fptr.read())
+    return rules
 
 
-def process_lines(source, colorizer, stream, term):
+def process_lines(source, parser):
     """
     Read lines from *source* and process them until an empty-line is read.
     """
@@ -85,8 +93,7 @@ def process_lines(source, colorizer, stream, term):
         line = source.readline()
         if not line:
             break
-        line = colorizer.process(line)
-        stream.write(line.format(t=term))
+        parser.feed(line)
 
 
 def create_pty(cmd, args):
@@ -95,12 +102,11 @@ def create_pty(cmd, args):
     return source
 
 
-def create_stdin(config_name, term):
+def create_stdin(config_name):
     source = sys.stdin
     if not config_name:
         print(
-            "${t.red}ERROR:${t.normal} When parsing stdin, you need to "
-            "specify a config file!".format(t=term),
+            "ERROR: When parsing stdin, you need to specify a config file!",
             file=sys.stderr,
         )
         sys.exit(9)
@@ -109,20 +115,18 @@ def create_stdin(config_name, term):
 
 def run(stream, args):
     args = parse_args(args)
-    term = Terminal()
 
     if args.cmd:
         cmd = basename(args.cmd[0])
-        colorizer = Colorizer.from_basename(cmd)
-        config_name = args.config_name or cmd
+        rules = load_config(args.config_name or cmd)
         cmdargs = args.cmd[1:]
         source = create_pty(cmd, cmdargs)
     else:
-        colorizer = Colorizer.from_config(args.config_name)
-        source = create_stdin(args.config_name, term)
-        config_name = args.config_name
+        source = create_stdin(args.config_name)
+        rules = load_config(args.config_name)
 
-    process_lines(source, colorizer, stream, term)
+    parser = Parser(rules, stream, ANSI)
+    process_lines(source, parser)
 
 
 def main():  # pragma: no cover
